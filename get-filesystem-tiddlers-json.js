fec19f8fd491ce99df25044603a7bac7137f035e
qom <qom@>
2020-01-05 17:45:58 -0500

    Identify new or removed tiddlers.

--- get-filesystem-tiddlers-json.js
+++ get-filesystem-tiddlers-json.js
@@ -22,6 +22,11 @@ exports.path = /^\/recipes\/default\/filesystem-tiddlers.json$/;
 exports.handler = function(request,response,state) {
    response.writeHead(200, {"Content-Type": "application/json"});
 	var tiddlerFiles = JSON.stringify(listTiddlerFiles($tw.boot.wikiTiddlersPath));
+    $tw.utils.each($tw.boot.files, f => {
+        if (!tiddlerFiles.includes(f.filepath)) {
+            console.log(f.filepath);
+        }
+    });
 	response.end(tiddlerFiles,"utf8");
 };
 
@@ -31,8 +36,33 @@ function listNewOrRemoved(state) {
     var loadedAtBoot = $tw.boot.files;
     //var originalFilePaths = state.wiki.getTiddler("$:/config/OriginalTiddlerPaths");
     //var originalFilePaths = $tw.boot.files;
-    var tiddlerFiles = listTiddlerFiles(tiddlerPath);
+    var tiddlersOnDisk = listTiddlerFiles(tiddlerPath);
     
+    var newOrRemoved = {};
+    var numCurrentlyLoadedFromDisk = 0;
+    $tw.utils.each($tw.boot.files, f => {
+        numCurrentlyLoadedFromDisk++;
+    });
+    
+    // Can't just compare numbers! Need to check each filename
+    var newOnDisk = [];
+    if (tiddlersOnDisk.length > numCurrentlyLoadedFromDisk) {
+        for (file in tiddlersOnDisk) {
+            if (!$tw.boot.files.file) {
+                newOnDisk.push(file);
+            } 
+        }
+    }
+    
+    //if(tiddlersOnDisk.length < )
+    
+    
+    if (!tiddlersOnDisk.includes(f.filepath)) {
+            console.log(f.filepath);
+        }
+    
+    /*
+    // Load missing tiddlers
     $tw.utils.each(tiddlerFiles, function(filepath) {
         var relativePath = filepath.replace($tw.boot.wikiTiddlersPath + "/", "");
         if (originalFilePaths.fields.text.indexOf(relativePath) == -1) {
@@ -50,6 +80,7 @@ function listNewOrRemoved(state) {
 		    $tw.wiki.addTiddlers(tiddlerFile.tiddlers);
         }
     })
+    */
     
 }
     
